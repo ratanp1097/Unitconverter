dae0c00518d05888450d6b789ab99f5cb596ec54
Ratan Potgantwar <ratanp1097@gmail.com>
2023-03-04 22:06:55 +0530

    https://github.com/ratanp1097/CodeClause_project_URL-Shortener.git

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-// Selecting DOM elements
-const tempCelsius = document.getElementById("temp-celsius");
-const tempFahrenheit = document.getElementById("temp-fahrenheit");
-const convertToFahrenheit = document.getElementById("convert-to-fahrenheit");
-const convertToCelsius = document.getElementById("convert-to-celsius");
-
-// Conversion functions
-function celsiusToFahrenheit(temp) {
-	return (temp * 9/5) + 32;
-}
-
-function fahrenheitToCelsius(temp) {
-	return (temp - 32) * 5/9;
-}
-
-// Event listeners for conversion buttons
-convertToFahrenheit.addEventListener("click", () => {
-	if (tempCelsius.value) {
-		tempFahrenheit.value = celsiusToFahrenheit(tempCelsius.value).toFixed(2);
-	} else {
-		alert("Please enter a temperature in Celsius");
-	}
-});
-
-convertToCelsius.addEventListener("click", () => {
-	if (tempFahrenheit.value) {
-		tempCelsius.value = fahrenheitToCelsius(tempFahrenheit.value).toFixed(2);
-	} else {
-		alert("Please enter a temperature in Fahrenheit");
-	}
-});
+function shortenUrl() {
+    var urlInput = document.getElementById("urlInput").value;
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var shortenedUrl = JSON.parse(this.responseText).shortUrl;
+        document.getElementById("shortenedUrl").innerHTML = shortenedUrl;
+      }
+    };
+    request.open("POST", "/api/shorten", true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(JSON.stringify({url: urlInput}));
+  }
+  
